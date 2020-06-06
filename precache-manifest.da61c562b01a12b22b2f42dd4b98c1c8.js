9b721abe56bd8e3de429cd865b1737e8cd40be59
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2020-06-06 15:49:43 -0300

    Updates

---
+++ precache-manifest.da61c562b01a12b22b2f42dd4b98c1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ea500c47b2420f51cd8d1e1978edad",
+    "url": "/Wiytiszen/beer-stock/index.html"
+  },
+  {
+    "revision": "014fb8f17c47b3161d1d",
+    "url": "/Wiytiszen/beer-stock/static/css/main.e125e2bd.chunk.css"
+  },
+  {
+    "revision": "f76b6610cd483d980151",
+    "url": "/Wiytiszen/beer-stock/static/js/2.befe3db6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wiytiszen/beer-stock/static/js/2.befe3db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014fb8f17c47b3161d1d",
+    "url": "/Wiytiszen/beer-stock/static/js/main.364c9424.chunk.js"
+  },
+  {
+    "revision": "97e44e3e55f37f48a481",
+    "url": "/Wiytiszen/beer-stock/static/js/runtime-main.4db27237.js"
+  }
+]);
